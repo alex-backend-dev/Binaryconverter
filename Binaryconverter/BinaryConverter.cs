ea69382a36d1ef373d869b36162153c6aa23b010
Alex Ulyanitskiy <adl821440@gmail.com>
2021-02-28 21:53:22 +0300

    Add project files.

---
+++ Binaryconverter/BinaryConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryConverter
+{
+    public static class BinaryConverter 
+    {
+        public static string StandartConvert(int number) 
+        {
+            return Convert.ToString(number, 2); 
+        }
+        public static string MyConvert(int number) 
+        {
+            string result = string.Empty; 
+            int modulo; 
+
+            while (true) 
+            {
+                if (number == 0) 
+                    break; 
+
+                modulo = number % 2; 
+                number /= 2; 
+                result += modulo; 
+            }
+            char[] symbols = result.ToCharArray();                                      
+            result = string.Empty; 
+
+            for (int i = symbols.Length - 1; i >= 0; i--)                                              
+            {
+                result += symbols[i]; 
+            }
+            return result; 
+        }
+    }
+}
---
+++ Binaryconverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BinaryConverter
+{
+    public class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            Console.Write("Введите число: "); 
+            int number = int.Parse(Console.ReadLine()); 
+
+            string binaryNumber = BinaryConverter.MyConvert(number); 
+
+            Console.WriteLine("Число в двоичной системе, используя свой собственный метод: " + binaryNumber);
+
+            binaryNumber = BinaryConverter.StandartConvert(number);
+            Console.WriteLine("Число в двоичной системе, используя стандартный метод: " + binaryNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
